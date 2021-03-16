a428686c711d757e2de3107d65f62a1df2fa1c0f
836662398 <836662398@qq.com>
2021-03-15 22:50:39 -0700

    fork

--- main.cpp
+++ main.cpp
@@ -33,31 +33,53 @@ void OnRequest(const HttpRequest& req, HttpResponse* resp) {
 }
 
 int main(int argc, char** argv) {
-    int num_thread = 4;
-    // parse args
+    int process_num = 2;
+    int sub_thread_num = 4;
+    int heartbeat_s = 0;  // parse args
     int opt;
-    const char *str = "t:b";
+    const char* str = "p:s:h:b";
     while ((opt = getopt(argc, argv, str)) != -1) {
         switch (opt) {
-            case 't': {
-                num_thread = atoi(optarg) - 1;
-                std::cout<<"Thread's num is "<<num_thread + 1<<std::endl;
+            case 'p': {
+                process_num = atoi(optarg);
+                break;
+            }
+            case 's': {
+                sub_thread_num = atoi(optarg);
                 break;
             }
             case 'b': {
+                benchmark = true;
                 spdlog::get("log")->set_level(spdlog::level::err);
-                std::cout<<"Benchmark starts."<<std::endl;
+                std::cout << "Benchmark starts." << std::endl;
                 break;
             }
             default:
                 break;
         }
     }
+    std::cout << "Process num is " << process_num << std::endl;
+    std::cout << "Sub thread's num is " << sub_thread_num << std::endl;
+    pid_t pid;
+    std::atomic<int> sequence_ = 0;
+    std::string name = "Server0";
+    printf("WebServer 0 starts\n");
+    for (int i = 1; i < process_num; i++) {
+        ++sequence_;
+        pid = fork();
+        if (pid == 0) {
+            printf("WebServer %d starts\n", sequence_.load());
+            fflush(stdout);
+            name = "Server" + std::to_string(sequence_);
+            break;
+        }
+        if (pid < 0) printf("fork() failed.");
+    }
     EventLoop loop;
-    HttpServer server(&loop, 80, num_thread, "WebServer");
+
+    HttpServer server(&loop, 8000, sub_thread_num, name, true);
     server.set_response_callback(OnRequest);
     server.StartListening();
-    std::cout << "Server starts" << std::endl;
     loop.Loop();
     return 0;
 }
